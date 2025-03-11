cf2e1a1bc9a602c5587ae197ef80a1c4861bf408
gface <89259112970@mail.ru>
2025-03-11 15:41:49 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+    }
+}
