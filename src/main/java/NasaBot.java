a0d01ffaa4d1192daadd5b9edc638f288dda04dc
gface <89259112970@mail.ru>
2025-03-30 15:56:54 +0300

    add: some fix

--- src/main/java/NasaBot.java
+++ src/main/java/NasaBot.java
@@ -68,14 +68,10 @@ public class NasaBot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
-
-
     @Override
     public String getBotUsername() {
         return botUserName;
     }
-
-
 
     @Override
     public String getBotToken() {
